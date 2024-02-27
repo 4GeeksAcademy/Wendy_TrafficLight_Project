3d25f2e5b335fac4adb08e32147a7fee47116f93
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-27 21:19:50 +0000

    5 o'clock commit

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -7,29 +7,78 @@ import { useState, useEffect } from "react";
 //create your first component
 export const TrafficLight= ()=> {
 
-    const [counter, setCounter] = useState(0);
+
+  const [countR, setCountR] = useState('gray');
+  const [countY, setCountY] = useState('gray');
+  const [countG, setCountG] = useState('gray');
    
+    
+    const [countS, setCountS] = useState(10);
 
-let  interval_sec;
 
+let  interval;
+let  interval2;
+let i = 1;
   useEffect(() => {
-     interval_sec = setInterval(() => {
+    // interval2 = setInterval(() => {
+    //   if(countS==0)
+    //   {
+    //     setCountS(10);
+    //   }
+    
+    //   else{
+    //     setCountS(countS-1);
+    //   }
+
+    // },1000);
+
+
+     interval = setInterval(() => {
+
+      if(i==1)
+      {     
+        setCountG('gray');
+        setCountR('red');
+        setCountY('gray');
+        console.log('This is the current position :'+i);    
+      i++;
+     
+        
+      }
+      else if(i==2){
+     
+          setCountG('gray');
+          setCountR('gray');
+          setCountY('yellow');
+      console.log('This is the current position :'+i);
+      i=3;
+      }
+        else if(i==3) {
+        
+          setCountG('green');
+          setCountR('gray');
+          setCountY('gray');   
+      
+         console.log('This is the current position :'+ i);
+     i=1;
+        }
     
   
-    }, 10000);
+    }, 5000);
 
+  
 
     return () => {
-      clearInterval(interval_sec);
+      clearInterval(interval);
    
     }
-  }, [counter]);
+  }, []);
 
 
 
 function stopCounter (){
  
-clearInterval(interval_sec);
+clearInterval(interval);
 
 
 }
@@ -37,15 +86,16 @@ clearInterval(interval_sec);
 
 
 
-    
+
     
 
 	return	(
         <div class="container">
         <div class="trafficlight">
-            <div class="redlight"></div>
-            <div class="yellowlight"></div>
-            <div class="greenlight"></div>
+            <div class="countlight"><span>{countS}</span></div>
+            <div class="redlight" style={{ backgroundColor:countR }}></div>
+            <div class="yellowlight" style={{ backgroundColor:countY }}></div>
+            <div class="greenlight" style={{ backgroundColor:countG}}></div>
             <div class="walklight"><i class="fa-solid fa-person-walking fa-2xl"></i></div>
 
             </div>
