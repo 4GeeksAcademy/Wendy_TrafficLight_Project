907e1829d3e64fcabaaaef85ce87b4eba87168b5
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-28 04:07:59 +0000

    functionality done

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -7,50 +7,55 @@ export const TrafficLight= ()=> {
 
   const [countR, setCountR] = useState('red');
   const [countY, setCountY] = useState('gray');
-  const [countG, setCountG] = useState('gray');   
-  const [countS, setCountS] = useState(5);
+  const [countG, setCountG] = useState('gray');  
+  const [countW, setCountW] = useState('gray');  
+  const [countS, setCountS] = useState(10);
   const [countI, setCountI] = useState(1);
-  
-  
+  const [count, setCount] = useState(0);
 
 let  interval;
-let  interval2;
 
   useEffect(() => {
  
      interval = setInterval(() => {
 
-    if(countS==1) { 
-           
-            setCountS(5);
+      if(countS==1 && count==1) { 
+        setCountG('gray');
+        setCountR('gray');
+        setCountY('gray');  
+        setCountW('white');
+        setCount(0);
+        setCountS(10);
+
+      }
+
+  else if(countS==1 && count==0) { 
+       setCountS(10);
 if(countI==1)
 {   
- 
   setCountG('gray');
   setCountR('gray');
   setCountY('yellow');  
+  setCountW('gray');
   setCountI(2);
-
 }
 
 else if(countI==2){
- 
+  setCountW('gray');
   setCountG('green');
   setCountR('gray'); 
   setCountY('gray');
   setCountI(3);
 
-
 }
   else if(countI==3) {
-   
+    setCountW('gray');
     setCountG('gray');
     setCountR('red');
     setCountY('gray');  
     setCountI(1);
 
   }
-  
       }
     
       else{
@@ -70,28 +75,9 @@ else if(countI==2){
 
 
 function CrossStreet (){
-
-console.log(countS);
-interval2 = setInterval(() => {
-
-if(countI==1)
-{   
-  setCountG('gray');
-  setCountR('gray');
-  setCountY('gray');  
- // setCountI(10);
- console.log('test cross button')
-}
-},1000);
-return() => {
-  clearInterval(interval2);
+setCount(1);
 
 }
-}
-
-
-
-
 
     
 
@@ -102,8 +88,8 @@ return() => {
             <div class="redlight" style={{ backgroundColor:countR }}></div>
             <div class="yellowlight" style={{ backgroundColor:countY }}></div>
             <div class="greenlight" style={{ backgroundColor:countG}}></div>
-            <div class="walklight"><i class="fa-solid fa-person-walking fa-2xl"></i></div>
-
+            <div class="walklight" style={{ backgroundColor:countW}}><i class="fa-solid fa-person-walking fa-2xl"></i></div>
+            
             </div>
             <button type="button" class="btn btn-secondary" onClick={()=>CrossStreet()}>Press the button if you want to cross</button>
      
